f3556b7a433e1d7bd39eb2629c32d154a7eb40b9
unknown <arthurrgspinto@gmail.com>
2025-06-16 16:20:49 -0300

    a

--- calculadoraEntrega.js
+++ calculadoraEntrega.js
@@ -25,6 +25,7 @@ console.log("Seja bem vindo à nossa Calculadora de Entrega! Quando quiser parar
 do {
     do {
         nome = promptSync(`Insira o nome do ${i + 1}º cliente: `);
+        
         if (nome === "@") {
             finalizacao = promptSync("Você optou por parar a aplicação. Tem certeza?\nY/N: "); //Verifica se o usuário quer realmente parar a aplicação.
 
@@ -33,19 +34,19 @@ do {
             } else {
                 nome = ""; //Força a repetição do Loop
             }
-        } else if (nome = "" || !isNaN(nome)) { //Verifica se o valor não é aplicável
+        } else if (nome == "" || !isNaN(nome)) { //Verifica se o valor não é aplicável
             console.log("Você inseriu um valor inválido, por favor, insira uma palavra sem números e não vazia.");
         } else { //Depois de todos os filtros, salva o input no vetor nomes.
             nomes[i] = nome;
         }
-    } while (nome = '' || !isNaN(nome))
+    } while (nome == "" || !isNaN(nome))
 
     if (finalizacao === "Y" || finalizacao === "y") { //Se o usuário optou por finalizar a aplicação, termina o loop
         break;
     }
     
     do {
-        endereco = (parseFloat(promptSync(`Digite o endereço de entrega do ${i + 1}º cliente: `)));
+        endereco = ((promptSync(`Digite o endereço de entrega do ${i + 1}º cliente: `)));
         if (!isNaN(endereco) || endereco === '') { //Se o endereço é um número ou é vazio
             console.log("Você inseriu um valor inválido. Por favor, insira uma frase não vazia."); //Deu errado
         } else {
@@ -55,12 +56,12 @@ do {
 
     do {
         distancia = (parseFloat(promptSync(`Digite a distância de entrega do vendedor até o endereço do ${i + 1}º cliente, em Km: `)));
-        if (isNaN(distancia) || distancia == "" || distancia <= 0) {
+        if (isNaN(distancia) || distancia === "" || distancia <= 0) {
             console.log("Você inseriu um valor inválido. Por favor, insira um número real positivo.");
         } else {
             distancias[i] = distancia; //Armazenou o valor no vetor
         }
-    } while (isNaN(distancia) || peso == "" || peso <= 0)
+    } while (isNaN(distancia) || distancia == "" || distancia <= 0)
 
     do {
         valorKM = (parseFloat(promptSync(`Digite o preço por quilômetro da ${i + 1}ª entrega: `)));
@@ -72,13 +73,13 @@ do {
     } while (isNaN(valorKM) || valorKM == "" || valorKM <= 0)
 
     do {
-        tipoEntrega = (parseFloat(promptSync(`Insira a urgencia da ${i + 1}ª entrega. Digite "R" para regular e "U" para urgente: `)));
-        if (!isNaN(tipoEntrega) || tipoEntrega == "" || tipoEntrega != "R" || tipoEntrega != "r" || tipoEntrega != "U" || tipoEntrega != "u") {
+        tipoEntrega = ((promptSync(`Insira a urgencia da ${i + 1}ª entrega. Digite "R" para regular e "U" para urgente: `)));
+        if (!isNaN(tipoEntrega) || tipoEntrega == "" ) { //Inserir depois || tipoEntrega != "R" || tipoEntrega != "r" || tipoEntrega != "U" || tipoEntrega != "u"
             console.log(`Você inseriu um valor inválido. Por favor, insira "R" para regular e "U" para urgente.`);
         } else if (tipoEntrega == "u" || tipoEntrega == "U" || tipoEntrega == "R" || tipoEntrega == "r") {
             tipoEntregas[i] = tipoEntrega; //Armazenou o valor no vetor
         }
-    } while (!isNaN(tipoEntrega) || tipoEntrega == "" || tipoEntrega != "R" || tipoEntrega != "r" || tipoEntrega != "U" || tipoEntrega != "u")
+    } while (!isNaN(tipoEntrega) || tipoEntrega === "" ) //Inserir depois || tipoEntrega != "R" || tipoEntrega != "r" || tipoEntrega != "U" || tipoEntrega != "u"
         i++;
 } while (finalizacao === "N" || finalizacao === "n")
 
@@ -91,12 +92,12 @@ for (let j = 0; j < i; j++) {
     custoEntregas[j] = custoEntrega;
 }
 
-if (!fs.existsSync("./Banco de Entregas")) {
-    fs.writeFileSync("./Banco de Entregas", "", 'utf-8');
+if (!fs.existsSync("./Banco_de_Entregas.txt")) {
+    fs.writeFileSync("./Banco_de_Entregas.txt", "", 'utf-8');
 }
 
 for (let h = 0; h < i; h++) {
     conteudo = `Entrega ${h + 1}\nNome do cliente: ${nomes[h]}.\nEndereço do Cliente: ${enderecos[h]}.\nDistância da Entrega: ${distancias[h]}Km.\nValor Cobrado por Km: ${valoresKM[h]}.\nTipo de Entrega: ${tipoEntregas[h]}.\nCUSTO FINAL DA ENTREGA: R$ ${custoEntregas[h]}.\n`;
     console.log(conteudo);
-    fs.appendFileSync("./Banco_de_dados.txt", `${conteudo} \n`);
+    fs.appendFileSync("./Banco_de_Entregas.txt", `${conteudo} \n`);
 }
