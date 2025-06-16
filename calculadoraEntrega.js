5df90e79b5f899633042e7dce4a7e9590fbcbee5
unknown <arthurrgspinto@gmail.com>
2025-06-16 14:34:05 -0300

    Definido o sistema de verificação de nomes, permitindo a finalização do loop em caso de digitação de @.

--- calculadoraEntrega.js
+++ calculadoraEntrega.js
@@ -2,13 +2,41 @@ const fs = require("fs");
 const promptSync = require('prompt-sync')();
 const path = require('path');
 
-nomes = []; //Nome do cliente.
-enderecos = [];// Endereço do cliente.
-distancias = []; // Distância da entrega (em quilômetros).
-valoresKM = [];// Valor cobrado por quilômetro.
-tipoEntregas = []; // Tipo de entrega (normal ou urgente).
-nome = "";
-endereco = '';
-distancia = 0;
-valorKM = 0;
-tipoEntrega = '';
+let nomes = []; //Nome do cliente.
+let enderecos = [];// Endereço do cliente.
+let distancias = []; // Distância da entrega (em quilômetros).
+let valoresKM = [];// Valor cobrado por quilômetro.
+let tipoEntregas = []; // Tipo de entrega (normal ou urgente).
+let nome = "";
+let endereco = '';
+let distancia = 0;
+let valorKM = 0;
+let tipoEntrega = '';
+let finalizacao = "N";
+let i = 0;
+
+//inputs 
+console.log("Seja bem vindo à nossa Calculadora de Entrega! Quando quiser parar a aplicação, insira um @ no valor de nome.");
+
+do {
+    do {
+        nome = promptSync(`Insira o nome do ${i + 1}º cliente: `);
+        if (nome === "@") {
+            finalizacao = promptSync("Você optou por parar a aplicação. Tem certeza?\nY/N: "); //Verifica se o usuário quer realmente parar a aplicação.
+
+            if (finalizacao === "Y" || finalizacao === "y") {
+                break; //Sai do loop
+            } else {
+                nome = ""; //Força a repetição do Loop
+            }
+        } else if (nome = "" || !isNaN(nome)) { //Verifica se o valor não é aplicável
+            console.log("Você inseriu um valor inválido, por favor, insira uma palavra sem números e não vazia.");
+        } else { //Depois de todos os filtros, salva o input no vetor nomes.
+            nomes[i] = nome;
+        }
+    } while (nome = '' || !isNaN(nome))
+
+    if (finalizacao === "Y" || finalizacao === "y") { //Se o usuário optou por finalizar a aplicação, termina o loop
+        break;
+    }
+} while (finalizacao === "N" || finalizacao === "n")
